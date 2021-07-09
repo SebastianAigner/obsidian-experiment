e548baa91eb5ebefa2a252bf30c3d36be066621b
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-07-09 22:36:55 +0300

    Fix for obsidian API

--- src/main/kotlin/simple.kt
+++ src/main/kotlin/simple.kt
@@ -1,14 +1,20 @@
 @OptIn(ExperimentalJsExport::class)
 @JsExport
 @JsName("default")
-class SomePlugin: Plugin() {
-//    override fun onload() {
-//        Notice("This is a notice!")
-//    }
-//
-//    override fun onunload() {
-//        Notice("Unloading plugin!")
-//    }
+class SomePlugin(
+    app: Any,
+    manifest: Any
+) : Plugin(
+    app,
+    manifest
+) {
+    override fun onload() {
+        Notice("This is a notice!")
+    }
+
+    override fun onunload() {
+        Notice("Unloading plugin!")
+    }
 }
 
 //fun main() {
